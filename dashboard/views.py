3159332fe158d244fba9f042242c89a74cd0445b
Parth <parthtripathi@thinbiz.co.in>
2020-12-18 04:11:21 +0530

    Added Fraud Detection model

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from fraud_detection import views as fraud_detectionViews
+import numpy as np
 # Create your views here.
 def index(request):
     return render(request, 'tableau.html')
@@ -17,9 +18,19 @@ def fraud_detection_pred(request):
     input2 = request.POST.get('InputText2')
     input3 = request.POST.get('InputText3')
     input4 = request.POST.get('InputText4')
-    print(input1,input2,input3,input4)
-    #pred_score = sentimentAnalysisViews.sentiment_analyser(input_string)
-    #print(pred_score)
-    #pred_score=pred_score*100
-    context={'input_string':input1}
+    input5 = request.POST.get('InputText5')
+    input6 = request.POST.get('InputText6')
+    input7 = request.POST.get('InputText7')
+    input8 = request.POST.get('InputText8')
+    input9 = request.POST.get('InputText9')
+    user_input=np.array([float(input1),float(input2),float(input3),float(input4),float(input5),float(input6),float(input7),float(input8),float(input9)]).reshape(1, -1)
+    print(user_input,type(user_input),user_input.shape)
+    prediction=fraud_detectionViews.make_pred(user_input)
+    print(prediction,len(prediction))
+    if prediction[0]==0:
+        prediction_text='Safe'
+    elif prediction[0]==0:
+        prediction_text='Fradulent'  
+    flag=len(prediction)    
+    context={'flag':flag,'prediction_text':prediction_text}
     return render(request, 'fraud_detection.html',context)
--- fraud_detection/views.py
+++ fraud_detection/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings as conf
 import pickle
+import os
 
 # Create your views here.
 def load_model():
-    model_path=os.path.join(conf.MODEL_DIR, './xgb_reg.pkl')
+    model_path=os.path.join(conf.MODELS_DIR, './xgb_model.pkl')
     pred_model=pickle.load(open(model_path, "rb"))
     return pred_model
 
@@ -11,5 +13,5 @@ pred_model=load_model()
 print('load success')
 
 def make_pred(user_input):
-    prediction = pred_model.predict(user_input)[1]
-        return prediction
+    prediction = pred_model.predict(user_input)
+    return prediction
