869f14eb59be9678351892f1ecab5c541ea460c0
Parth <parthtripathi@thinbiz.co.in>
2020-12-18 13:33:08 +0530

    Added retraining module

--- fraud_detection/views.py
+++ fraud_detection/views.py
@@ -2,8 +2,46 @@ from django.shortcuts import render
 from django.conf import settings as conf
 import pickle
 import os
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+from sklearn.preprocessing import LabelEncoder
+#import pyodbc
+
+
 
 # Create your views here.
+def model_retrain():
+    host = '74.91.21.100'
+    port = 31010
+    uid = 'letaptikna'
+    pwd = '9039396188as'
+    driver = "Dremio Connector"
+    cnxn=pyodbc.connect("Driver={};ConnectionType=Direct;HOST={};PORT={};AuthenticationType=Plain;UID={};PWD={}".format(driver,host,port,uid,pwd),autocommit=True)
+    sql = '''SELECT * FROM "@letaptikna".clean_ny'''
+    df = pd.read_sql(sql,cnxn)
+    df = df.rename(columns={'oldbalanceOrg':'oldBalanceOrig', 'newbalanceOrig':'newBalanceOrig', \
+                            'oldbalanceDest':'oldBalanceDest', 'newbalanceDest':'newBalanceDest'})
+    df = df.drop(['isFlaggedFraud'],axis=1)
+    df['errorBalanceOrig'] = df.newBalanceOrig + df.amount - df.oldBalanceOrig
+    df['errorBalanceDest'] = df.oldBalanceDest + df.amount - df.newBalanceDest
+    df = df.drop(['nameOrig','nameDest'], axis=1)
+    enc = LabelEncoder()
+    df['type'] = enc.fit_transform(df['type'])
+    Y = df.isFraud
+    X = df.drop(['isFraud'],axis=1)
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(X.values,Y.values,test_size=0.2, random_state = 1)
+    # scale_pos_weight should be ratio of negative classes to positive classes
+    weights = (Y == 0).sum() / (Y == 1).sum()
+    clf = XGBClassifier(max_depth = 3, scale_pos_weight = weights, n_jobs = 4)
+    clf.fit(Xtrain,Ytrain)
+    pickle.dump(clf, open(os.path.join(conf.MODELS_DIR, './xgb_model.pkl'), "wb"))
+    print('Retraining Successful')
+
+
+
+
 def load_model():
     model_path=os.path.join(conf.MODELS_DIR, './xgb_model.pkl')
     pred_model=pickle.load(open(model_path, "rb"))
